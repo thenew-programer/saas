114a22961a6025309b0605f46884576bb5fd12d7
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-12-22 16:49:06 +0100

    add railway domain to allowed hosts

--- src/batisync/settings.py
+++ src/batisync/settings.py
@@ -25,7 +25,14 @@ SECRET_KEY = "django-insecure-)hs2n3tg8ky&l7k7i^zwa$#a$&-gzeb+mxl%#o=h1kq%!90^eg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".railway.app",
+]
+if DEBUG:
+    ALLOWED_HOSTS += [
+        "localhost",
+        "127.0.0.1",
+    ]
 
 
 # Application definition
